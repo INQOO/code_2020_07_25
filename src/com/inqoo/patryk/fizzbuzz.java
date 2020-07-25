af040ecb3f36654f10aae02c26f7e8628e28275d
Lenovo <patryk_st@wp.pl>
2020-07-25 12:32:47 +0200

    Add FizzBuzz Done

--- src/com/inqoo/patryk/fizzbuzz.java
+++ src/com/inqoo/patryk/fizzbuzz.java
@@ -3,5 +3,17 @@ package com.inqoo.patryk;
 public class fizzbuzz {
     public static void main(String[] args) {
 
+        for (int i = 1; i<= 50; i++){
+
+            if (i % 3 == 0 & i % 5 ==0){
+                System.out.println("FizzBuzz");
+            }else if ((i % 3) ==0){
+                System.out.println("Fizz");
+            }else if ((i % 5) ==0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
+        }
     }
 }
