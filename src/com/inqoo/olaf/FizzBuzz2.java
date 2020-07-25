604d645056bd10715c896884b4193e5f0963d4bb
Lenovo <patryk_st@wp.pl>
2020-07-25 14:02:25 +0200

    Add FizzBuzz2 Done

--- src/com/inqoo/olaf/FizzBuzz2.java
+++ src/com/inqoo/olaf/FizzBuzz2.java
@@ -1,10 +1,26 @@
 package com.inqoo.olaf;
 
+import java.util.Scanner;
+
 public class FizzBuzz2 {
     public static void main(String[] args) {
         // zapytac uzytkownika ile liczb wypisac
         // zapisujecie to do zmiennej
         // wypisujecie od 1 do liczby ktora podal uzytkownik
         // uzywamy petli while
+        int number;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Odliczanie do podanej liczby: ");
+            number = scanner.nextInt();
+        }
+        System.out.println("Liczba: " + number);
+        int counter = 0;
+
+        while(counter <= number){
+            System.out.println("Nadal w pętli licznik: " + counter);
+            counter ++;
+        }
+        System.out.println("Koniec pętli !!!");
+
     }
 }
