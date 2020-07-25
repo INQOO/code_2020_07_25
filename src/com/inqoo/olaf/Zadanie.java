1fe8fb70a55d62a83c3eb8802dce783a0725881f
Olaf Goj <olaf@goj.email>
2020-07-25 16:08:50 +0200

    Add Zadanie

---
+++ src/com/inqoo/olaf/Zadanie.java
@@ -0,0 +1,25 @@
+package com.inqoo.olaf;
+
+class Zadanie {
+    static void main(String[] args) {
+        /*
+        1. wypisz menu:
+            1. Zamiana stringa na duze litery
+            2. Zamiana stringa na male litery
+            3. Usuniecie ze stringa bialych znakow (z poczatku i konca)
+            4. Sprawdzi czy podany string jest pusty
+            5. Wypisze n poczatkowych znakow stringa
+
+        2. switch(wybor_uzytkownika)
+        1 .... changeToBigLetters() { toUpperCase
+        2 ....
+        default "Wybrales zle"
+
+        1
+        Podaj stringa: Olaf
+        OLAF
+
+        10 - Wybrales zle
+         */
+    }
+}
