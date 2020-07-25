3dd5a05f08c2f0270eb171a1ce124f8d9bcb352f
Olaf Goj <olaf@goj.email>
2020-07-25 13:46:29 +0200

    Add FizzBuzz2 instructions

---
+++ src/com/inqoo/olaf/FizzBuzz2.java
@@ -0,0 +1,10 @@
+package com.inqoo.olaf;
+
+public class FizzBuzz2 {
+    public static void main(String[] args) {
+        // zapytac uzytkownika ile liczb wypisac
+        // zapisujecie to do zmiennej
+        // wypisujecie od 1 do liczby ktora podal uzytkownik
+        // uzywamy petli while
+    }
+}
